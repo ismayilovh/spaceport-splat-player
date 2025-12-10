72868c15978701698c7ca6edfea7c6bfc0e71343
Hamit Mermerkaya <hamit@antmedia.io>
2025-12-10 11:34:41 +0000

    Edit scene-config.ts

--- src/scene-config.ts
+++ src/scene-config.ts
@@ -29,7 +29,7 @@ const sceneConfig = {
     controls: {
         dampingFactor: 0.2,
         minPolarAngle: Math.PI/4,
-        maxPolarAngle: Math.PI/1.8,
+        maxPolarAngle: Math.PI/1.9,
         minZoom: 1e-6,
         maxZoom: 10.0,
         initialAzim: -45,
